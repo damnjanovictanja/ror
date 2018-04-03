699d2197ad0adf3f32629d7802bcca98ccdfe7e8
Tanja Damnjanovic <damnjanovic.tanja96@gmail.com>
2018-04-03 21:47:18 +0200

    Regex improved

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -16,20 +16,19 @@ class InvitationsController < ApplicationController
 		message = params[:invitation][:message]
 		valid = true
 		@invalids = []
-		all_emails.split(/\s*,\s*/).each do |email|
+		all_emails.split(/\s+|(\s*,\s*)/).each do |email|
 			valid = false unless(email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
 		end
 		if valid
-			all_emails.split(/\s*,\s*/).each do |email|
+			all_emails.split(/\s+|(\s*,\s*)/).each do |email|
 				raw_parameters = {:email => email, :message => message}
- puts raw_parameters
 				parameters = ActionController::Parameters.new(raw_parameters)
 				@invitation = Invitation.new(parameters.permit(:email, :message))
 				@invitation.save
 			end
 			redirect_to @invitation
 		else
-			all_emails.split(/\s*,\s*/).each do |email|
+			all_emails.split(/\s+|(\s*,\s*)/).each do |email|
 				@invalids.push(email) unless (email =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
 			end
 			raw_parameters = {:email => all_emails, :message => message}
